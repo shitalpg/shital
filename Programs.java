89ef4e3d6a3e070187fa90d86dbe44bbdd69eb5f
shitalpg <99710751+shitalpg@users.noreply.github.com>
2022-02-15 12:15:18 +0530

    Update Programs.java

--- Programs.java
+++ Programs.java
@@ -1,6 +1,8 @@
+Q1. program  to perform basic calculator operation
+=>
 import java.util.Scanner;
 
-class program
+class prog1
 {
   public static void main(String[] args) 
 {
@@ -55,4 +57,71 @@ class program
 
     input.close();
   }
+}
+Q2.program to calculate fabonice series upto n numbers
+=>
+import java.util.Scanner;
+class prog2
+{
+  public static void main(String[] args) 
+{
+     Scanner sc=new Scanner(System.in);
+    int n , firstTerm = 0, secondTerm = 1;
+     n=sc.nextInt();
+    System.out.println("Fibonacci Series till " + n + " terms:");
+
+    for (int i = 1; i <= n; ++i) {
+      System.out.print(firstTerm + ", ");
+
+      // compute the next term
+      int nextTerm = firstTerm + secondTerm;
+      firstTerm = secondTerm;
+      secondTerm = nextTerm;
+    }
+  }
+}
+
+Q3. Program to calculate factorial number
+=>
+import java.util.Scanner; 
+public class prog3
+{    
+  Public static void main(String args[])
+{       int i =1, factorial=1, number;      
+ System.out.println("Enter the number to which you need to find the factorial:");       
+Scanner sc = new Scanner(System.in);      
+ number = sc.nextInt();      
+ while(i <=number)
+ {         
+     factorial = factorial * i;         
+     i++;       
+}       
+System.out.println("Factorial of the given number is:: "+factorial);   
+ } 
+}
+
+Q4. Program to find string is palindrome or not
+=>
+ public class prog4
+{
+    public static void main(String args[])
+    {
+        String a, b = "";
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the string you want to check:");
+        a = s.nextLine();
+        int n = a.length();
+        for(int i = n - 1; i >= 0; i--)
+        {
+            b = b + a.charAt(i);
+        }
+        if(a.equalsIgnoreCase(b))
+        {
+            System.out.println("The string is palindrome.");
+        }
+        else
+        {
+            System.out.println("The string is not palindrome.");
+        }
+    }
 }
